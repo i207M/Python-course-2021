c068b3080e41a0b3931a3700f47bd38c56d91dee
i207M <connect@mail.i207m.top>
2021-09-01 09:56:11 +0800

    ck all reply

--- temp.py
+++ temp.py
@@ -1,27 +1,15 @@
 import glob
+import json
 import os
 import requests
 
 bv_list = open('clawed_bv').read().splitlines()
-print(len(bv_list))
-s = set()
-cnt_a = 0
-cnt_b = 0
-for bv in bv_list:
-    if not os.path.exists(f'clawed/{bv}.json'):
-        print(bv)
-        cnt_a += 1
-    else:
-        s.add(bv)
-        cnt_b += 1
-print(cnt_a, cnt_b)
-print(len(s))
 
-s2 = set()
-cnt = 0
-for file in glob.glob('clawed/*.json'):
-    # print(file)
-    s2.add(file.split('\\')[1][:-5])
-    cnt += 1
-print(cnt)
-print(s - s2)
+for bv in bv_list:
+    path = f'clawed/{bv}.json'
+    if os.path.exists(path):
+        dat = json.load(open(path, 'r', encoding='utf-8'))
+        if 'reply' not in dat:
+            print(f'No reply: {bv}')
+    else:
+        print(f'No json: {bv}')

5de981e3c4190f2f0af25abf72444822f47fb9a7
i207M <connect@mail.i207m.top>
2021-09-02 10:53:35 +0800

    main as aid

--- temp.py
+++ temp.py
@@ -5,11 +5,14 @@ import requests
 
 bv_list = open('clawed_bv').read().splitlines()
 
+mxlen = 0
+
 for bv in bv_list:
     path = f'clawed/{bv}.json'
     if os.path.exists(path):
         dat = json.load(open(path, 'r', encoding='utf-8'))
-        if 'reply' not in dat:
-            print(f'No reply: {bv}')
+        mxlen = max(len(dat['title']), mxlen)
     else:
         print(f'No json: {bv}')
+
+print(mxlen)

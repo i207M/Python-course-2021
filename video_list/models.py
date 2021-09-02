0872cdbd2c64002cd6087c295612344fddded4cf
i207M <connect@mail.i207m.top>
2021-09-02 11:00:37 +0800

    migrate

--- video_list/models.py
+++ video_list/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+    aid = models.IntegerField()
+    bvid = models.CharField(max_length=15)
+    title = models.CharField(max_length=100)
+    desc = models.TextField()
+
+    def __str__(self):
+        return f'video_{self.aid}'

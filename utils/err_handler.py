5aa5de9c82c9cdd53a79566f1d22b61f4912f747
i207M <connect@mail.i207m.top>
2021-09-04 17:24:05 +0800

    base, 404

--- utils/err_handler.py
+++ utils/err_handler.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-def err404(request, err_msg: str = '404 Not Found'):
+def err404(request, err_msg: str = 'Not Found'):
     return render(request, '404.html', context={'err_msg': err_msg}, status=404)
